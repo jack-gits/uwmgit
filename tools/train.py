8c492f175453f1fa9700d1dedf0afe1abff47ca0
za-xuhao001 <hao.xu@zatech.com>
2022-05-22 00:52:19 +0800

    add

--- tools/train.py
+++ tools/train.py
@@ -12,13 +12,13 @@ from mmcv.cnn.utils import revert_sync_batchnorm
 from mmcv.runner import get_dist_info, init_dist
 from mmcv.utils import Config, DictAction, get_git_hash
 
-from organ_segmentation.MMsegmentation.Kaggle_UWMGIT.mmseg import __version__
-from organ_segmentation.MMsegmentation.Kaggle_UWMGIT.mmseg.apis import set_random_seed, train_segmentor
+from uwmgit.mmseg import __version__
+from uwmgit.mmseg.apis import set_random_seed, train_segmentor
 # from mmseg.datasets import build_dataset
-from organ_segmentation.MMsegmentation.Kaggle_UWMGIT.mmseg.models import build_segmentor
-from organ_segmentation.MMsegmentation.Kaggle_UWMGIT.mmseg.utils import collect_env, get_root_logger
+from uwmgit.mmseg.models import build_segmentor
+from uwmgit.mmseg.utils import collect_env, get_root_logger
 
-from organ_segmentation.MMsegmentation.Kaggle_UWMGIT.mmseg.datasets import build_dataset
+from uwmgit.mmseg.datasets import build_dataset
 
 
 def parse_args():
